f903e748dde87b740e0b39f7db3e39fad5043479
abrugxllardo <vagrant@primerParcial>
2024-10-15 01:28:22 +0000

    feat: Script para poder particionar el disco, formatear y montar las particiones de manera persistente

--- RTA_SCRIPT_Examen_20241012/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241012/Punto_B.sh
@@ -20,3 +20,13 @@ sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2
 sudo mount /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3
 sudo mount /dev/sdc11 /Examenes-UTN/profesores/
 df -h
+echo '/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc2 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc3 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc5 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc6 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc7 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc8 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc9 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc10 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc11 /Examenes-UTN/profesores ext4 defaults 0 0' | sudo tee -a /etc/fstab
